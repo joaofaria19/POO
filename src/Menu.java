c8628158e39784f9d3c7e52bbc1e2fd4eb783b62
ederzito88 <tiagoluis.sporting@sapo.pt>
2022-04-29 15:21:24 +0100

    Parsing de dados a partir de texto

--- src/Menu.java
+++ src/Menu.java
@@ -10,13 +10,13 @@ public class Menu {
         sb.append("#                                        #\n");
         sb.append("# Selecione uma das seguintes opções:    #\n");
         sb.append("#                                        #\n");
-        sb.append("# (1) Registar um novo SmartDevice       #\n");
-        sb.append("# (2) Registar uma nova Divisão          #\n");
-        sb.append("# (3) Verificar SmartDevices existentes  #\n");
-        sb.append("# (4) Verificar Divisões existentes      #\n");
-        sb.append("# (5) Verificar Fornecedores existentes  #\n");
-        sb.append("# (6) Registar um novo Fornecedor        #\n");
-        sb.append("# (6) Carregar um estado                 #\n");
+        //sb.append("# (1) Registar um novo SmartDevice       #\n");
+        //sb.append("# (2) Registar uma nova Divisão          #\n");
+        sb.append("# (1) Verificar Casas existentes         #\n");
+        //sb.append("# (4) Verificar Divisões existentes      #\n");
+        sb.append("# (2) Verificar Fornecedores existentes  #\n");
+        sb.append("# (3) Registar um novo Fornecedor        #\n");
+        sb.append("# (4) Carregar um estado                 #\n");
         sb.append("# (0) Sair                               #\n");
         sb.append("##########################################\n");
         System.out.println(sb.toString());
@@ -27,6 +27,7 @@ public class Menu {
     public static void options(int op) throws FileNotFoundException {
         switch(op){
             case 0:
+                System.out.println("feitinho, adeus");
                 break;
             case 1:
                 System.out.println("op 1");
@@ -36,14 +37,20 @@ public class Menu {
                 System.out.println("op 2");
                 break;
             case 3:
+                //Menu.parsing();
+                System.out.println("op 3");
+                break;
+            case 4:
 
-                System.out.println("O que pretende?\n\n(1) Carregar ficheiro de texto\n(2) Carregar ficheiro binário");
+                System.out.println("O que pretende?\n\n(1) Carregar ficheiro de texto\n(2) Carregar ficheiro binário\n(0)Voltar");
                 Scanner scanner = new Scanner(System.in);
                 int next = scanner.nextInt();
-                Menu.parsing(next);
+                if(next == 0) Menu.MenuInicial();
+                else Menu.parsing(next);
                 break;
             default:
                 System.out.println("Insira uma opção válida");
+                Menu.MenuInicial();
         }
     }
 
@@ -55,17 +62,55 @@ public class Menu {
                 while (scanner.hasNext()) {
                     String token = scanner.next();
                     //ntoken++;
-                    String[] parts = token.split(":");
-                    String part1 = parts[0];
-                    String part2 = parts[1];
-                    System.out.printf("%s%n", token);
-                    System.out.printf("%s%n", parts[0]);
+                    String[] parts = token.split("\n");
+
+                    for(int j = 0; j < parts.length; j++) {
+                        //String part1 = parts[j];
+                       // String[] line = token.split(":");
+                        //String part2 = parts[1];
+                        //System.out.printf("%s%n", token);
+
+                        String[] type = parts[j].split(":");
+                        //System.out.printf("%s%n", type[0]);
+                        Menu.parseLine(type[0], type[1]);
+                    }
                 }
             }
         }
         else{
             //carregar de binário
         }
+
+
+    }
+    public static void parseLine(String type, String args){
+        //String[] line = token.split(":");
+        //System.out.printf("%s%n %s%n", type, args);
+
+        switch(type){
+            case "Fornecedor":
+                System.out.println("é fornecedor");
+                break;
+            case "Casa":
+                System.out.println("é casa");
+                break;
+            case "Divisao":
+                System.out.println("é divisao");
+                break;
+            case "SmartBulb":
+                System.out.println("é smartbulb");
+                break;
+            case "SmartCamera":
+                System.out.println("é camara");
+                break;
+            case "SmartSpeaker":
+                System.out.println("é speaker");
+                break;
+            default:
+                System.out.println("default");
+                break;
+        }
+
     }
 }
 
