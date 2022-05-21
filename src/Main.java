9ebc4c87689b0051f66e2f501b4cea7fe8b0f93d
ederzito88 <tiagoluis.sporting@sapo.pt>
2022-05-21 16:44:03 +0100

    FEITINHO

--- src/Main.java
+++ src/Main.java
@@ -5,30 +5,5 @@ public class Main {
     public static void main(String[] args) throws IOException, ObjectNullException, ClassNotFoundException, ObjectEmpty {
         Controller.run();
 
-        /*CasaInteligente casa1 = new CasaInteligente();
-        SmartCamera sc1 = new SmartCamera("sc1",5.2,1024);
-        SmartBulb sb1 = new SmartBulb("sb1",2.5,10);
-        SmartSpeaker sp1 = new SmartSpeaker("sp1","TVI",15,Marca.Sonos);
-        SmartSpeaker sp2 = new SmartSpeaker("sp2","SIC",12,Marca.Marshall);
-        SmartCamera sc2 = new SmartCamera("sc2",4.3,1024);
-        casa1.addDevice(sb1);
-        casa1.addDevice(sp1);
-        casa1.addDevice(sc1);
-        casa1.addDevice(sc2);
-        casa1.addDevice(sp2);
-        double sct = sc1.consumoDispositivo();
-        double sbt = sb1.consumoDispositivo();
-        double spt = sp1.consumoDispositivo();
-        casa1.addRoom("sala");
-        casa1.addRoom("quarto");
-        casa1.addToRoom("sala", "sp1");
-        casa1.addToRoom("sala", "sb1");
-        casa1.addToRoom("sala", "sc1");
-        casa1.addToRoom("quarto", "sc2");
-        casa1.addToRoom("quarto", "sp2");
-
-         */
-
-        //Menu.options(resultado);
     }
 }
